12bb50a9dfd08b70d6d89e4a15701aa77730e2a5
nlaz <nikolazaris@utexas.edu>
2015-07-24 17:09:40 -0400

    refactor

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log("HEY GIRL...")
+  console.log("HEY LOON...")
   chrome.tabs.executeScript(null, {
-    file: "heygirl.js"
+    file: "heyloon.js"
   });
 });
